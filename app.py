4cd2ce2f9eaca2014692479c04844698505d0ad3
zhangruochi <zrc720@gmail.com>
2022-05-04 03:33:07 +0800

    add comments

--- app.py
+++ app.py
@@ -139,37 +139,37 @@ if uploaded_file is not None:
 
 # Comments part
 
-# conn = db.connect()
-# comments = db.collect(conn)
+conn = db.connect()
+comments = db.collect(conn)
 
-# with st.expander("💬 Open comments"):
+with st.expander("💬 Open comments"):
 
-#     # Show comments
+    # Show comments
 
-#     st.write("**Comments:**")
+    st.write("**Comments:**")
 
-#     for index, entry in enumerate(comments.itertuples()):
-#         st.markdown(COMMENT_TEMPLATE_MD.format(
-#             entry.name, entry.date, entry.comment))
+    for index, entry in enumerate(comments.itertuples()):
+        st.markdown(COMMENT_TEMPLATE_MD.format(
+            entry.name, entry.date, entry.comment))
 
-#         is_last = index == len(comments) - 1
-#         is_new = "just_posted" in st.session_state and is_last
-#         if is_new:
-#             st.success("☝️ Your comment was successfully posted.")
+        is_last = index == len(comments) - 1
+        is_new = "just_posted" in st.session_state and is_last
+        if is_new:
+            st.success("☝️ Your comment was successfully posted.")
 
-#     space(2)
+    space(2)
 
-#     # Insert comment
+    # Insert comment
 
-#     st.write("**Add your own comment:**")
-#     form = st.form("comment")
-#     name = form.text_input("Name")
-#     comment = form.text_area("Comment")
-#     submit = form.form_submit_button("Add comment")
+    st.write("**Add your own comment:**")
+    form = st.form("comment")
+    name = form.text_input("Name")
+    comment = form.text_area("Comment")
+    submit = form.form_submit_button("Add comment")
 
-#     if submit:
-#         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-#         db.insert(conn, [[name, comment, date]])
-#         if "just_posted" not in st.session_state:
-#             st.session_state["just_posted"] = True
-#         st.experimental_rerun()
+    if submit:
+        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        db.insert(conn, [[name, comment, date]])
+        if "just_posted" not in st.session_state:
+            st.session_state["just_posted"] = True
+        st.experimental_rerun()
